03a09026699464bb2963463c0539358c7305fd39
John Jablonski <john.jablonski@eventstag.com>
2016-10-24 10:28:42 +0100

    First commit, main app skeleton

---
+++ app.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+// app init
+var app = express();
+
+// logging dependencies
+var morgan = require('morgan');
+var FileStreamRotator = require('file-stream-rotator');
+var fs = require('fs');
+
+// logging setup
+var logDirectory = __dirname + '/log';
+var accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYY-MM-DD',
+    filename: logDirectory + '/access-%DATE%.log',
+    frequency: 'daily',
+    verbose: false
+});
+app.use(morgan('combined', {
+    stream: accessLogStream
+}));
+
+// request parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// routes configuration
+var routes = require('./routes/index');
+var printers = require('./routes/printers');
+
+app.use('/', routes);
+app.use('/printers', printers);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
